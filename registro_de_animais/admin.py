3ae304756d189f90fc23e770983ba0cb6f8fd1d1
PAULO HENRIQUE DA SILVA NETO <86234861+PauloHSNeto@users.noreply.github.com>
2023-05-11 06:53:55 -0300

    update

--- registro_de_animais/admin.py
+++ registro_de_animais/admin.py
@@ -4,11 +4,6 @@ from django.contrib import admin
 from .models.animais import *
 from .models.vacinas import *
 
-admin.site.register(Tutor)
-admin.site.register(Especie)
-admin.site.register(Sexo)
-admin.site.register(Porte)
-admin.site.register(Raça)
 admin.site.register(Animal)
 
 admin.site.register(Vacina)
--- registro_de_animais/models/animais.py
+++ registro_de_animais/models/animais.py
@@ -3,56 +3,18 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
-class Tutor(models.Model):
-    nome = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField()
-
-    def __str__(self):
-        return self.nome.username
-
-
-class Especie(models.Model):
-    nome = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.nome
-
-
-class Sexo(models.Model):
-    nome = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.nome
-
-
-class Porte(models.Model):
-    nome = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.nome
-
-
-class Raça(models.Model):
-    nome = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.nome
-
-
 class Animal(models.Model):
-    id = models.AutoField(primary_key=True)
-    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
+    tutor = models.CharField(max_length=50)
+    nome = models.CharField(max_length=50)
     idade = models.IntegerField()
-    especie = models.ForeignKey(Especie, on_delete=models.CASCADE)
-    sexo = models.ForeignKey(Sexo, on_delete=models.CASCADE)
+    especie = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=50)
     data_nascimento = models.DateField()
     data_cadastro = models.DateField(auto_now_add=True)
     cor = models.CharField(max_length=50)
     foto = models.ImageField()
-    porte = models.ForeignKey(Porte, on_delete=models.CASCADE)
-    raça = models.ForeignKey(Raça, on_delete=models.CASCADE)
+    porte = models.CharField(max_length=50)
+    raça = models.CharField(max_length=50)
     rga = models.CharField(max_length=50)
     castrado = models.BooleanField()
     anilha = models.CharField(max_length=50)
