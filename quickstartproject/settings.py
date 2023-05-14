fce4aacdd42fe68edb1d6d3d146ac571fc94cdef
PAULO HENRIQUE DA SILVA NETO <86234861+PauloHSNeto@users.noreply.github.com>
2023-05-14 11:46:57 -0300

    Update settings.py

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -92,6 +92,10 @@ DATABASES = {
         'OPTIONS': {
             'sslmode': 'require',
         }
+    },
+    'dev': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
