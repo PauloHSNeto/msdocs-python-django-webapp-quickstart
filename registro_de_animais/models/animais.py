7ca643477d9393163ab55d5211511dde0faea69f
Graciel Covanzi <41657320+Gracov@users.noreply.github.com>
2023-05-15 20:41:46 -0300

    continuação da refatoração front end

--- registro_de_animais/models/animais.py
+++ registro_de_animais/models/animais.py
@@ -10,18 +10,18 @@ class Animal(models.Model):
     ani_espec = models.CharField(max_length=50)
     ani_sexo = models.CharField(max_length=50)
     ani_cor = models.CharField(max_length=50)
-    ani_foto = models.ImageField()
+    ani_foto = models.ImageField(blank=True)
     ani_porte = models.CharField(max_length=50)
-    ani_rga = models.CharField(max_length=50)
+    ani_rga = models.CharField(max_length=50, blank=True)
     ani_castr = models.CharField(max_length=50)
-    ani_anilha = models.CharField(max_length=50, default='não informado')
-    ani_nmchip = models.CharField(max_length=50, default='não informado')
-    ani_dnasc = models.DateField()
-    ani_idade = models.IntegerField()
+    ani_anilha = models.CharField(max_length=50, default='não informado', blank=True)
+    ani_nmchip = models.CharField(max_length=50, default='não informado', blank=True)
+    ani_dnasc = models.DateField(blank=True)
+    ani_idade = models.IntegerField(blank=True)
     data_cadastro = models.DateField(auto_now_add=True)
-    ani_vacinado = models.BooleanField()
-    ani_vermifugado = models.BooleanField()
-    ani_obs = models.CharField(max_length=200, default='não informado')
+    ani_vacinado = models.BooleanField(blank=True)
+    ani_vermifugado = models.BooleanField(blank=True)
+    ani_obs = models.CharField(max_length=200, default='não informado', blank=True)
 
     def __str__(self):
-        return self.name
+        return self.ani_nome
