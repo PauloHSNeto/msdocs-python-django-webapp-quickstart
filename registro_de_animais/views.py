0f36ed6ff7f2b8888fcadc8f198cdcda35cc61a7
Graciel Covanzi <41657320+Gracov@users.noreply.github.com>
2023-05-20 09:45:54 -0300

    revisão views e formulário de cadastro e alteração

--- registro_de_animais/views.py
+++ registro_de_animais/views.py
@@ -5,6 +5,7 @@ from .forms import SignUpForm, AddAnimalForm, UpdateAnimalForm
 from .models.animais import *
 from django.contrib.auth.decorators import login_required
 
+
 def home(request):
 	animal = Animal.objects.all()
 	# Check to see if logging in
@@ -25,69 +26,60 @@ def home(request):
 
 
 def login_user(request):
-	records = Animal.objects.all()
-	# Check to see if logging in
-	if request.method == 'POST':
-		username = request.POST['username']
-		password = request.POST['password']
-		# Authenticate
-		user = authenticate(request, username=username, password=password)
-		if user is not None:
-			login(request, user)
-			messages.success(request, "Você está logado...")
-			return redirect('home')
-		else:
-			messages.success(request, "Houve um erro ao tentar logar...Tente novamente...")
-			return redirect('login')
-	else:
-		return render(request, 'login.html', {'animais':records})
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Você está logado.")
+            return redirect('home')
+        else:
+            messages.error(request, "Usuário ou senha inválidos. Por favor, tente novamente.")
+            return redirect('login')
+
+    return render(request, 'login.html')
 
 
+@login_required
 def logout_user(request):
-	logout(request)
-	messages.success(request, "Você saiu da sua conta...")
-	return redirect('home')
+    logout(request)
+    messages.success(request, "Você saiu da sua conta...")
+    return redirect('home')
 
 
 def register_user(request):
-	if request.method == 'POST':
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			form.save()
-			# Authenticate and login
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password1']
-			user = authenticate(username=username, password=password)
-			login(request, user)
-			messages.success(request, "Você foi registrado com sucesso, Bem vindo!")
-			return redirect('home')
-	else:
-		form = SignUpForm()
-		return render(request, 'register.html', {'form':form})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            
+            messages.success(request, "Você foi registrado com sucesso. Bem-vindo!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'register.html', {'form': form})
 
-	return render(request, 'register.html', {'form':form})
 
-
-
+@login_required
 def registro_do_animal(request, pk):
-	if request.user.is_authenticated:
-		animais = Animal.objects.get(id=pk)
-		return render(request, 'record.html', {'animais':animais})
-	else:
-		messages.success(request, "Voce precisa estar logado para fazer isso...")
-		return redirect('home')
+    animal = Animal.objects.get(id=pk)
+    return render(request, 'record.html', {'animal': animal})
 
 
-
+@login_required
 def delete_animal(request, pk):
-	if request.user.is_authenticated:
-		delete_it = Animal.objects.get(id=pk)
-		delete_it.delete()
-		messages.success(request, "Animal Deletado com sucesso!")
-		return redirect('home')
-	else:
-		messages.success(request, "Você precisa estar logado para fazer isso...")
-		return redirect('home')
+    delete_it = Animal.objects.get(id=pk)
+    delete_it.delete()
+    messages.success(request, "Animal deletado com sucesso!")
+    return redirect('home')
 
 
 @login_required
@@ -123,18 +115,16 @@ def update_animal(request, pk):
     return render(request, 'update_animal.html', {'form': form})
 
 
+@login_required
 def pet_list(request):
-	if request.user.is_authenticated:
-		animals = Animal.objects.all()
-		return render(request, 'pet_list.html', {'animals':animals})
-	else:
-		messages.success(request, "Você precisa estar logado para fazer isso...")
-		return redirect('home')
+    animals = Animal.objects.all()
+    return render(request, 'pet_list.html', {'animals': animals})
 
+@login_required
 def animal_profile(request, pk):
-	if request.user.is_authenticated:
-		animal = Animal.objects.get(id=pk)
-		return render(request, 'animal_profile.html', {'animal':animal})
-	else:
-		messages.success(request, "Você precisa estar logado para fazer isso...")
-		return redirect('home')
+    try:
+        animal = Animal.objects.get(id=pk)
+        return render(request, 'animal_profile.html', {'animal': animal})
+    except Animal.DoesNotExist:
+        messages.error(request, "Animal não encontrado.")
+        return redirect('home')
